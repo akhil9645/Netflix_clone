30eef246b728e75ed9977822ac35030057d2f37c
Akhil Jose <akhiljose735@gmail.com>
2023-07-20 21:28:55 +0530

    changed the name of nowplaying folder

---
+++ lib/domain/now_playing/now_playing_api.dart
@@ -0,0 +1,25 @@
+import 'dart:convert';
+
+import 'package:netflix_clone/core/constants/api_constants.dart';
+import 'package:http/http.dart' as http;
+import 'package:netflix_clone/domain/now_playing/now_playing_mode/now_playing_model.dart';
+import 'package:netflix_clone/domain/now_playing/now_playing_mode/result.dart';
+
+Future<List<Result>> getNowPlayingImgs() async {
+  var uri = "$domain/movie/now_playing?api_key=$apikey}";
+  var url = Uri.parse(uri);
+  var response = await http.get(url);
+  if (response.statusCode == 200) {
+    var nowPlaybody = response.body;
+    var jsonBody = jsonDecode(nowPlaybody);
+    var nowPlayData = NowplayingModel.fromJson(jsonBody);
+    if (nowPlayData.results != null) {
+      return nowPlayData.results!;
+    }
+  } else {
+    print(response.statusCode);
+  }
+
+  List<Result> empty = [];
+  return empty;
+}
